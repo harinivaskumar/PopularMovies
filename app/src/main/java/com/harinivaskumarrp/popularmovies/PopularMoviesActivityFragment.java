ef0f805bc2c4e4a11ecf0a52901cfb973d7ae3d9
harinivaskumar <harinivaskumar@live.com>
2015-11-28 00:54:45 +0530

    FetchMovieTask - Modularizing Network Connection
    Network Connection to tmdb.org is moved to appropriate functions.

--- app/src/main/java/com/harinivaskumarrp/popularmovies/PopularMoviesActivityFragment.java
+++ app/src/main/java/com/harinivaskumarrp/popularmovies/PopularMoviesActivityFragment.java
@@ -19,7 +19,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
 
@@ -31,7 +33,7 @@ public class PopularMoviesActivityFragment extends Fragment {
     private final String LOG_TAG = PopularMoviesActivityFragment.class.getSimpleName();
 
     // please, add in your movie database APP ID from www.themoviedb.org
-    private String MY_MOVIE_APP_ID = "Your Personal TMDB.org APP ID";
+    private String MY_MOVIE_APP_ID = "Your Personal APP_ID";
 
     public PopularMoviesActivityFragment() {
     }
@@ -65,14 +67,14 @@ public class PopularMoviesActivityFragment extends Fragment {
         ConnectivityManager connectivityManager
                 = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        if (activeNetworkInfo != null && activeNetworkInfo.isConnected()){
+        if (activeNetworkInfo != null && activeNetworkInfo.isConnected()) {
             return true;
         }
         Log.e(LOG_TAG, "No Internet Connection available.");
         return false;
     }
 
-    private void updateMovieList(){
+    private void updateMovieList() {
         int pageNumber = 1;
         int sortByType = 2;
         int minVoteCount = 100;
@@ -81,11 +83,11 @@ public class PopularMoviesActivityFragment extends Fragment {
         movieTask.execute();
     }
 
-    public void onStart(){
+    public void onStart() {
         super.onStart();
-        if (isNetworkAvailable()){
+        if (isNetworkAvailable()) {
             updateMovieList();
-        }else{
+        } else {
             Toast.makeText(getContext(),
                     "You are Offline.\nPlease, check your Network Connection!",
                     Toast.LENGTH_LONG)
@@ -106,21 +108,37 @@ public class PopularMoviesActivityFragment extends Fragment {
         private final String POPULARITY_DESC = "popularity.desc";
         private final String HIGHEST_RATING = "vote_average.desc";
 
+        private final int DISCOVER_MOVIE = 1;
+
         private final int POPULARITY = 1;
         private final int RATING = 2;
 
-        private final int SUCCESS_HTTP_OK = 200;
-        private final int CLIENT_ERROR_BAD_REQUEST = 400;
+        private final int HTTP_REQUEST_FAILURE = 0;
+        private final int HTTP_SUCCESS_HTTP_OK = 200;
+        private final int HTTP_CLIENT_ERROR_BAD_REQUEST = 400;
 
+        int mUrlHeadType = 0;
         int mPageNumber = 0;
         int mSortByType = 0;
         int mMinVoteCount = 0;
         int mResponseCode = 0;
+        String mResponseMessage = null;
 
-        public FetchMovieTask (int pageNumber, int sortByType, int minVoteCount) {
+        public FetchMovieTask(int pageNumber, int sortByType, int minVoteCount) {
+            setUrlHeadType(DISCOVER_MOVIE);
             setPageNumber(pageNumber);
             setSortByType(sortByType);
             setMinVoteCount(minVoteCount);
+            setResponseCode(HTTP_REQUEST_FAILURE);
+            setResponseMessage(null);
+        }
+
+        private int getUrlHeadType() {
+            return mUrlHeadType;
+        }
+
+        private void setUrlHeadType(int urlHeadType) {
+            this.mUrlHeadType = urlHeadType;
         }
 
         private int getPageNumber() {
@@ -147,111 +165,160 @@ public class PopularMoviesActivityFragment extends Fragment {
             this.mMinVoteCount = minVoteCount;
         }
 
-        public int getResponseCode() {
+        private int getResponseCode() {
             return mResponseCode;
         }
 
-        public void setResponseCode(int responseCode) {
+        private void setResponseCode(int responseCode) {
             this.mResponseCode = responseCode;
         }
 
-        protected Uri getMovieUri (){
-            if (getSortByType() == POPULARITY) {
-                return (Uri.parse(DISCOVER_MOVIE_BASE_URL).buildUpon()
-                        .appendQueryParameter(PAGE_PARAM, getPageNumber() + "")
-                        .appendQueryParameter(SORT_PARAM, POPULARITY_DESC)
-                        .appendQueryParameter(APPID_PARAM, MY_MOVIE_APP_ID)
-                        .build());
-            }else{
-                return (Uri.parse(DISCOVER_MOVIE_BASE_URL).buildUpon()
-                        .appendQueryParameter(PAGE_PARAM, getPageNumber() + "")
-                        .appendQueryParameter(VOTE_COUNT_PARAM, getMinVoteCount() + "")
-                        .appendQueryParameter(SORT_PARAM, HIGHEST_RATING)
-                        .appendQueryParameter(APPID_PARAM, MY_MOVIE_APP_ID)
-                        .build());
-            }
+        private String getResponseMessage() {
+            return mResponseMessage;
         }
 
-        protected boolean checkResponseCode (HttpURLConnection urlConnection)
-                throws IOException{
+        private void setResponseMessage(String responseMessage) {
+            this.mResponseMessage = responseMessage;
+        }
 
-            // Referred Android API guide
-            setResponseCode(urlConnection.getResponseCode());
-            String responseMessage = urlConnection.getResponseMessage();
+        private Uri getTmdbUri() {
 
-            if (getResponseCode() == CLIENT_ERROR_BAD_REQUEST){
-                Log.e(LOG_TAG_I, "Response: " + responseMessage + "/" + mResponseCode);
-                return false;
-            }else if(getResponseCode() == SUCCESS_HTTP_OK){
-                Log.d(LOG_TAG_I, "Response: " + responseMessage + "/" + mResponseCode);
+            String baseURLStr = null;
+
+            if (getUrlHeadType() == DISCOVER_MOVIE) {
+                baseURLStr = DISCOVER_MOVIE_BASE_URL;
+
+                if (getSortByType() == POPULARITY) {
+                    return (Uri.parse(baseURLStr).buildUpon()
+                            .appendQueryParameter(PAGE_PARAM, getPageNumber() + "")
+                            .appendQueryParameter(SORT_PARAM, POPULARITY_DESC)
+                            .appendQueryParameter(APPID_PARAM, MY_MOVIE_APP_ID)
+                            .build());
+                } else if (getSortByType() == RATING) {
+                    return (Uri.parse(baseURLStr).buildUpon()
+                            .appendQueryParameter(PAGE_PARAM, getPageNumber() + "")
+                            .appendQueryParameter(VOTE_COUNT_PARAM, getMinVoteCount() + "")
+                            .appendQueryParameter(SORT_PARAM, HIGHEST_RATING)
+                            .appendQueryParameter(APPID_PARAM, MY_MOVIE_APP_ID)
+                            .build());
+                }
+            }
+            return null;
+        }
+
+        private URL getTmdbURLInstance() {
+
+            URL tmdbURL = null;
+
+            try {
+                Uri tmdbUri = getTmdbUri();
+
+                if (tmdbUri == null) {
+                    Log.e(LOG_TAG_I, "URI build Failed");
+                } else {
+                    String tmdbURLStr = URLDecoder.decode(tmdbUri.toString(), "UTF-8");
+                    Log.d(LOG_TAG_I, "TMDB URL : " + tmdbURLStr);
+
+                    tmdbURL = new URL(tmdbURLStr);
+                }
+            } catch (UnsupportedEncodingException | MalformedURLException e) {
+                Log.e(LOG_TAG_I, "getTmdbURLInstance : UnsupportedEncodingException/MalformedURLException");
+                e.printStackTrace();
+            }
+            return tmdbURL;
+        }
+
+        private boolean checkResponseCode(InputStream inputErrorStream)
+                throws IOException {
+
+            if (getResponseCode() == HTTP_CLIENT_ERROR_BAD_REQUEST) {
+                Log.d(LOG_TAG_I, "Response: " + mResponseMessage + "/" + mResponseCode);
+
+                String jsonErrorStr =
+                        readJSONStringFromIOStream(inputErrorStream);
+
+                Log.d(LOG_TAG_I, "Json Error String - " + jsonErrorStr);
+            } else if (getResponseCode() == HTTP_SUCCESS_HTTP_OK) {
+                Log.d(LOG_TAG_I, "Response: " + mResponseMessage + "/" + mResponseCode);
                 return true;
             }
             return false;
+        }
+
+        private String readJSONStringFromIOStream(InputStream inputStream) {
+
+            BufferedReader bufferedReader = null;
+            String jsonStr = null;
+
+            if (inputStream == null) {
+                Log.e(LOG_TAG_I, "readJSONStringFromIOStream : InputStream is Null");
+            } else {
+                try {
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    bufferedReader = new BufferedReader(inputStreamReader);
+                    StringBuffer bufferStr = new StringBuffer();
+                    String lineStr = "";
+
+                    while ((lineStr = bufferedReader.readLine()) != null) {
+                        bufferStr.append(lineStr);
+                    }
+
+                    jsonStr = bufferStr.toString();
+                } catch (IOException e) {
+                    Log.e(LOG_TAG_I, "readJSONStringFromIOStream : IOException");
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        if (bufferedReader != null) {
+                            bufferedReader.close();
+                        }
+                    } catch (IOException e) {
+                        Log.e(LOG_TAG_I, "readJSONStringFromIOStream : IOException");
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return jsonStr;
         }
 
         @Override
         protected String doInBackground(Void... params) {
 
             HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-            String movieJsonStr = null;
+            String jsonStr = null;
 
             try {
-                Uri movieUri = getMovieUri();
-
-                String discoverMovieURL = URLDecoder.decode(movieUri.toString(), "UTF-8");
-                //Log.d(LOG_TAG_I, "discoverMovieURL : " + discoverMovieURL);
-
-                URL url = new URL(discoverMovieURL);
+                URL url = getTmdbURLInstance();
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
 
-                if (!checkResponseCode(urlConnection)){
+                // Referred Android API guide
+                setResponseCode(urlConnection.getResponseCode());
+                setResponseMessage(urlConnection.getResponseMessage());
+
+                if (!checkResponseCode(urlConnection.getErrorStream())) {
                     return null;
                 }
 
-                InputStream inputStream = urlConnection.getInputStream();
-                if (inputStream == null){
-                    Log.e(LOG_TAG_I, "InputStream is Null!");
-                    return null;
-                }
-
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-
-                while ((line = reader.readLine()) != null) {
-                    buffer.append(line);
-                }
-
-                movieJsonStr = buffer.toString();
-                //Log.d(LOG_TAG_I, "MovieList Json String - " + movieJsonStr);
+                jsonStr = readJSONStringFromIOStream(urlConnection.getInputStream());
+                Log.d(LOG_TAG_I, "Json String - " + jsonStr);
             } catch (IOException e) {
-                Log.e(LOG_TAG_I, "IOException");
+                Log.e(LOG_TAG_I, "doInBackground : IOException");
                 e.printStackTrace();
             } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
                 }
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                    Log.e(LOG_TAG_I, "IOException");
-                    e.printStackTrace();
-                }
             }
-
-            return movieJsonStr;
+            return jsonStr;
         }
 
         @Override
         protected void onPostExecute(String strings) {
             super.onPostExecute(strings);
 
-            if (getResponseCode() == CLIENT_ERROR_BAD_REQUEST) {
+            if (getResponseCode() == HTTP_CLIENT_ERROR_BAD_REQUEST) {
                 Toast.makeText(getContext(),
                         "Invalid API key!\nYou must be granted a valid key.",
                         Toast.LENGTH_SHORT)
